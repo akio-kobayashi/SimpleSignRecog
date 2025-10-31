4148e4aacec46d8b154f4b70a61d55fc7993134c
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2025-10-31 12:52:44 +0900

    random selection

--- create_metadata_subset.py
+++ create_metadata_subset.py
@@ -1,10 +1,19 @@
+from argparse import ArgumentParser
 import pandas as pd
+
+parser = ArgumentParser()
+parser.add_argument("--max_num", type=int, default=20, help="１クラスあたりの最大サンプル数")
+args = parser.parse_args()
 
 # Load the metadata
 df = pd.read_csv("metadata.csv")
 
-# Group by class_label and take the first 20 samples from each group
-subset_df = df.groupby("class_label").head(20)
+# Group by class_label and take the first max_num samples from each group
+#subset_df = df.groupby("class_label").head(args.max_num)
+subset_df = (
+    df.groupby("class_label", group_keys=False)
+      .apply(lambda x: x.sample(n=min(len(x), args.max_num), random_state=42))
+)
 
 # Save the subset to a new CSV file
 subset_df.to_csv("metadata_subset.csv", index=False)
